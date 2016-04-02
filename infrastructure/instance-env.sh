e68f9face4974c187352e5190897a6da179a165b
csiron <csiron13@live.com>
2016-04-01 20:34:19 -0500

    fixed name of file and mv line in file

---
+++ infrastructure/instance-env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
+
+git clone https://github.com/ITMT-430/photoshare.git
+
+sudo mv photoshare/* /var/www/html
