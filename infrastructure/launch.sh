8d32db978a3cb313874a78889cb234106b87ca13
csiron <csiron13@live.com>
2016-04-01 20:12:55 -0500

    changed 3 instances to 4 and added echo explaining sleep

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -2,12 +2,9 @@
 
 echo "Check the README file for help if needed"
 
-declare -a instancesARR
+euca-run-instances -g default -k $1 -t c1.medium -n 4 emi-c87b2863
 
-mapfile -t instancesARR < <(euca-run-instances -g default -k $1 -t c1.medium -n 3 emi-c87b2863 --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
-
-echo ${instancesARR[@]}
-
+echo "short sleep to allow instances to load before creating the load balancer"
 sleep 30
 
 eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
