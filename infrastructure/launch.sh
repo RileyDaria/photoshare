47f32fdf270a9272869832d54ca1369bae07dcb1
csiron <csiron13@live.com>
2016-04-01 20:02:27 -0500

    updated to make it three instances that launch and launch an ELB

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-euca-run-instances -g default -k $1 -t c1.medium -n 3 emi-c87b2863 
+declare -a instancesARR
+
+mapfile -t instancesARR < <(euca-run-instances -g default -k $1 -t c1.medium -n 3 emi-c87b2863 --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+
+echo ${instancesARR[@]}
+
+sleep 30
+
+eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
