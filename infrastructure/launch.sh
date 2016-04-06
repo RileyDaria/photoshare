c6d76691f99d44afa73b01a1960fa7c559eaecec
csiron <csiron13@live.com>
2016-04-06 01:01:29 -0500

    added stuff for instance id

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+declare -a instancesARR
+
 echo "Check the README file for help if needed"
 
-euca-run-instances -g default -k $1 -t c1.medium -n 4 -d instance-env.sh emi-c87b2863
+mapfile -t instancesARR < <(euca-run-instances -g default -k $1 -t c1.medium -n 4 -d instance-env.sh emi-c87b2863 | grep INSTANCE | sed "s/|//g" | tr -d ' ' | sed "s/INSTANCE//g")
+
+echo ${instancesARR[@]}
 
 echo "short sleep to allow instances to load before creating the load balancer"
 sleep 30
