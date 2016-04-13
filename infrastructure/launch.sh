cbe113a11abd08f3560755819a34515881a4ed02
csiron <csiron13@live.com>
2016-04-12 23:58:42 -0500

    commented out lines for testing purposes

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -4,11 +4,11 @@ declare -a instancesARR
 
 echo "Check the README file for help if needed"
 
-mapfile -t instancesARR < <(euca-run-instances -g default -k $1 -t c1.medium -n 4 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep INSTANCE | sed "s/|//g" | tr -d ' ' | sed "s/INSTANCE//g")
+mapfile -t instancesARR < <(euca-run-instances -g default -k $1 -t c1.medium -n 4 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep instance | sed "s/|//g" | tr -d ' ' | sed "s/instance//g")
 
 echo ${instancesARR[@]}
 
-echo "short sleep to allow instances to load before creating the load balancer"
+#echo "short sleep to allow instances to load before creating the load balancer"
 sleep 30
 
-eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
+#eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
