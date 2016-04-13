d3fbb8b798917d8edfa0db826ffe3a9a2d656044
csiron <csiron@users.noreply.github.com>
2016-04-13 00:02:27 -0500

    commented out another line i missed for testing

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -9,6 +9,6 @@ mapfile -t instancesARR < <(euca-run-instances -g default -k $1 -t c1.medium -n 
 echo ${instancesARR[@]}
 
 #echo "short sleep to allow instances to load before creating the load balancer"
-sleep 30
+##sleep 30
 
 #eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
