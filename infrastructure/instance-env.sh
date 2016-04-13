d69b3aa0dac82e1c28cbd012a25315f76279fe0c
csiron <csiron@users.noreply.github.com>
2016-04-12 23:38:38 -0500

    added mv index

--- infrastructure/instance-env.sh
+++ infrastructure/instance-env.sh
@@ -7,6 +7,7 @@ git clone https://github.com/ITMT-430/photoshare.git
 
 sudo mv photoshare/* /var/www/html
 cd /var/www/html/navrozwork
+sudo mv index.html ../
 sudo mv signup.html ../
 sudo mv style1.css ../
 sudo mv style.css ../
