a04d1b65cc263abc278850bc9afecd519a85a128
csiron <csiron@users.noreply.github.com>
2016-04-12 23:27:05 -0500

    added vim to install command

--- infrastructure/instance-env.sh
+++ infrastructure/instance-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql vim
 
 git clone https://github.com/ITMT-430/photoshare.git
 
