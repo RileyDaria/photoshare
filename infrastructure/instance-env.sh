511a1244b8885ab3e88427385c1e44b3fc482e9f
csiron <csiron@users.noreply.github.com>
2016-04-12 23:30:59 -0500

    added commands to move files out of navroz folder

--- infrastructure/instance-env.sh
+++ infrastructure/instance-env.sh
@@ -6,3 +6,7 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql 
 git clone https://github.com/ITMT-430/photoshare.git
 
 sudo mv photoshare/* /var/www/html
+cd /var/www/html/navrozwork
+sudo mv signup.html ../
+sudo mv style1.css ../
+sudo mv style.css ../
