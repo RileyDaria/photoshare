6d60627807077d19f7bf79624a43022d968ac160
csiron <csiron@users.noreply.github.com>
2016-04-14 23:13:00 -0500

    added more install command and lines for testing

--- infrastructure/db-env.sh
+++ infrastructure/db-env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql vim
+sudo apt-get install -y git php5 php5-curl mysql-client curl php5-mysql vim
 
 git clone https://github.com/ITMT-430/photoshare.git
+
+#export DEBIAN_FRONTEND=noninteractive
+#sudo -E apt-get -q -y install mysql-server
 
 mysql -u user -p < photoshare/php/127_0_0_1.sql
