0bb7590ff714685cfe321ea1f662ae9c739d4460
csiron <csiron13@live.com>
2016-04-19 23:10:38 -0500

    updated grep

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -4,7 +4,7 @@ declare -a instancesARR
 
 echo "Check the README file for help if needed"
 
-mapfile -t instancesARR < <(euca-run-instances -g group2 -k $1 -t c1.medium -n 1 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep instance | sed "s/|//g" | tr -d ' ' | sed "s/instance//g")
+mapfile -t instancesARR < <(euca-run-instances -g group2 -k $1 -t c1.medium -n 1 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep -o 'i-.\{0,8\}' | head -1)
 
 echo ${instancesARR[@]}
 
