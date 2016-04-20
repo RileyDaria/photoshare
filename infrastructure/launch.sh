82f01e8347b3e030c302fb92d82696901665f58f
csiron <csiron13@live.com>
2016-04-19 23:29:55 -0500

    added sleep to script

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -4,7 +4,7 @@ declare -a instancesARR
 
 echo "Check the README file for help if needed"
 
-mapfile -t instancesARR < <(euca-run-instances -g group2 -k $1 -t c1.medium -n 2 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep -o 'i-.\{0,8\}' | head -1)
+mapfile -t instancesARR < <(euca-run-instances -g group2 -k $1 -t c1.medium -n 1 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep -o 'i-.\{0,8\}' | head -1)
 
 echo ${instancesARR[@]}
 
@@ -13,4 +13,7 @@ sleep 30
 
 eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
 
-eulb-register-instances-with-lb --instances instancesARR[1], instancesARR[2] hawkstagram-elb
+echo "another short sleep to allow the load balancer to be created before attaching instances to it"
+sleep 120
+
+eulb-register-instances-with-lb --instances instancesARR[@] hawkstagram-elb
