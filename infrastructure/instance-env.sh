1a45e46052b044e45e51094c85da205ff453d375
csiron <csiron@users.noreply.github.com>
2016-04-25 14:04:57 -0500

    commenting lines for testing

--- infrastructure/instance-env.sh
+++ infrastructure/instance-env.sh
@@ -10,7 +10,7 @@ sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 git clone https://github.com/ITMT-430/photoshare.git
 
 sudo mv photoshare/* /var/www/html
-cd /var/www/html/navrozwork
-sudo mv *.html ../
-sudo mv *.css ../
-sudo mv *.jpg ../
+#cd /var/www/html/navrozwork
+#sudo mv *.html ../
+#sudo mv *.css ../
+#sudo mv *.jpg ../
