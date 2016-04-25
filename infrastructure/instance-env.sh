55ac1d7d90679c5c9e53ba01982d80b9eb355735
csiron <csiron@users.noreply.github.com>
2016-04-25 10:43:43 -0500

    added lines for composer

--- infrastructure/instance-env.sh
+++ infrastructure/instance-env.sh
@@ -3,6 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql vim
 
+sudo curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
 git clone https://github.com/ITMT-430/photoshare.git
 
 sudo mv photoshare/* /var/www/html
