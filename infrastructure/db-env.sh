914e0c521dfc8b97d9ce689d690eec28c6e4dcac
csiron <csiron@users.noreply.github.com>
2016-04-26 23:50:45 -0500

    added lines for php 7

--- infrastructure/db-env.sh
+++ infrastructure/db-env.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+sudo apt-get install software-properties-common -y 
+sudo apt-get install python-software-properties -y
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo apt-get update -y
-sudo apt-get install -y git php5 php5-curl mysql-client curl php5-mysql vim
+sudo apt-get purge php5-common -y
+sudo apt-get install php7.0 php7.0-fpm php7.0-mysql -y
+sudo apt-get --purge autoremove -y
+
+sudo apt-get update -y
+sudo apt-get install -y git php-curl mysql-client curl vim
 
 git clone https://github.com/ITMT-430/photoshare.git
 
