bfba0515c16016ef5e9115a2754ed6b6bdaa1036
csiron <csiron13@live.com>
2016-04-26 22:00:12 -0500

    added address association lines

--- infrastructure/launch.sh
+++ infrastructure/launch.sh
@@ -11,13 +11,17 @@ mapfile -t instancesARR < <(euca-run-instances -g group2 -k $1 -t c1.medium -n 1
 
 mapfile -t instbackupARR < <(euca-run-instances -g group2 -k $1 -t c1.medium -n 1 -f photoshare/infrastructure/instance-env.sh emi-c87b2863 | grep -o 'i-.\{0,8\}' | head -1)
 
-echo "Main isntance id"
-echo ${instbackupARR[@]}
 echo "backup instance id"
+echo ${instbackupARR[@]}
+echo "main instance id"
 echo ${instancesARR[@]}
 
 echo "short sleep to allow instances to load before creating the load balancer"
 sleep 30
+
+euca-associate-address --instance ${instancesARR[@]} 64.131.111.50
+
+euca-associate-address --instance  ${instbackupARR[@]} 64.131.111.51
 
 eulb-create-lb -z RICE01 -l "lb-port=80, protocol=HTTP, instance-port=80, instance-protocol=HTTP" hawkstagram-elb
 
